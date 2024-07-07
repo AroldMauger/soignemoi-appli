853d781b67165476767d0dbdf9a79b64146999c6
Arold Mauger <aroldmauger2022@gmail.com>
2024-07-07 16:30:15 +0200

    Mise en forme de la page de connexion de l'application

--- app/src/main/java/com/soignemoi/doctorapp/MainActivity.kt
+++ app/src/main/java/com/soignemoi/doctorapp/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.soignemoi.doctorapp.login
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import androidx.appcompat.app.AppCompatActivity
 import com.soignemoi.doctorapp.BuildConfig
-import kotlinx.android.synthetic.main.activity_main.loginButton
-import kotlinx.android.synthetic.main.activity_main.identification
-import kotlinx.android.synthetic.main.activity_main.lastname
-import org.koin.android.ext.android.inject
 import com.soignemoi.doctorapp.R
 import com.soignemoi.doctorapp.dashboard.DashboardActivity
+import kotlinx.android.synthetic.main.activity_main.identification
+import kotlinx.android.synthetic.main.activity_main.lastname
+import kotlinx.android.synthetic.main.activity_main.loginButton
+import kotlinx.android.synthetic.main.activity_main.titleofapp
+import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
     private val viewModel by inject<LoginViewModel>()
@@ -17,6 +21,39 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Hide the ActionBar
+        if (supportActionBar != null) {
+            supportActionBar?.hide()
+        }
+
+        // Set up colored text
+        val text = "L’application réservée aux médecins de SoigneMoi"
+        val spannableString = SpannableString(text)
+
+        val startSoigne = text.indexOf("Soigne")
+        val endSoigne = startSoigne + "Soigne".length
+        val startMoi = text.indexOf("Moi")
+        val endMoi = startMoi + "Moi".length
+
+        val greenColor = resources.getColor(R.color.green)
+        val orangeColor = resources.getColor(R.color.orange)
+
+        spannableString.setSpan(
+            ForegroundColorSpan(greenColor),
+            startSoigne,
+            endSoigne,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        spannableString.setSpan(
+            ForegroundColorSpan(orangeColor),
+            startMoi,
+            endMoi,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        titleofapp.text = spannableString
 
         setupListeners()
         if (BuildConfig.DEBUG) {
